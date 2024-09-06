a1b1e543aaf9d3a0ad2a7fe07986be361475ea28
Phelipe_Silvestre <phelipe17silvestre@gmail.com>
2024-09-05 21:06:35 -0300

    desafio 5

---
+++ 05.-interruptor.js
@@ -0,0 +1,8 @@
+/*5) Você está em uma sala com três interruptores, cada um conectado a uma lâmpada em salas diferentes. Você não pode ver as lâmpadas da sala em que está, mas pode ligar e desligar os interruptores quantas vezes quiser. Seu objetivo é descobrir qual interruptor controla qual lâmpada. Como você faria para descobrir, usando apenas duas idas até uma das salas das lâmpadas, qual interruptor controla cada lâmpada?  
+*/
+
+// ----------------------------------------->><<-------------------------------------------------------------------
+
+// Resposta:
+
+// Como esta definido que posso ligar o interruptor quantas vezes quiser, e não foi dito que não pode ligar todos interruptores ao mesmo tempo, eu ligaria os 3 de uma vez e depois iria até as salas.
